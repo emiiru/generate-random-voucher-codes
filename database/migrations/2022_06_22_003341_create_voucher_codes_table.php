bb15dee4afc483102bbb48840bb8d325c7d8bd4f
emil_tamayo <zen4emil@gmail.com>
2022-06-22 09:11:14 +0800

    Added Voucher Codes Migration File

---
+++ database/migrations/2022_06_22_003341_create_voucher_codes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoucherCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voucher_codes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('code', 12)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voucher_codes');
+    }
+}
